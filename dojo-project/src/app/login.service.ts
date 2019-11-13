23ec5e13342ed11e67f680c4a8bfb378d0dd35e4
Douglas Cunha <douglas.cunha@dtidigital.com.br>
2019-11-13 14:41:00 -0300

    Criando service

---
+++ dojo-project/src/app/login.service.ts
@@ -0,0 +1,9 @@
+import { HttpClient,  HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+export class LoginService {
+
+}
